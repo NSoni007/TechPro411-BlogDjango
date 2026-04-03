d7ba40f83d591b378c3e8c0a631311d10f0811d1
nsoni007 <narindersoni75@gmail.com>
2026-04-03 11:41:39 +0200

    Updated the URL and folder name

--- user-data.sh
+++ user-data.sh
@@ -19,17 +19,23 @@ curl -sS https://bootstrap.pypa.io/get-pip.py | python3.8
 cd /home/ubuntu/
 
 # Clone your repo (CHANGE THIS)
-git clone https://github.com/xxxxxxxxxxxxx/xxxxxxxxxxxxxxx.git
+#git clone https://github.com/xxxxxxxxxxxxx/xxxxxxxxxxxxxxx.git
+### UPDATED ###
+git clone https://github.com/NSoni007/TechPro411-BlogDjango.git
 
 # Enter project folder (CHANGE THIS)
-cd /home/ubuntu/xxxxxxxxxxxxxx/BlogDjango
+# cd /home/ubuntu/xxxxxxxxxxxxxx/BlogDjango
+### UPDATED ###
+cd /home/ubuntu/TechPro411-BlogDjango/BlogDjango
 
 # Install requirements using pip3.8
 python3.8 -m pip install --upgrade pip
 python3.8 -m pip install --break-system-packages -r requirements.txt
 
 # Go to Django src folder (CHANGE THIS)
-cd /home/ubuntu/xxxxxxxxxxxxxxxxx/BlogDjango/src
+# cd /home/ubuntu/xxxxxxxxxxxxxxxxx/BlogDjango/src
+### UPDATED ###
+cd /home/ubuntu/TechPro411-BlogDjango/BlogDjango/src
 
 # Run Django commands
 python3.8 manage.py collectstatic --noinput
